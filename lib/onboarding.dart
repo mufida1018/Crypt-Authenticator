aaa0dced8c9441a18c26bb3b0b61384a44a3d088
mufida1018 <mufidapatel03@gmail.com>
2022-10-09 17:01:55 +0530

    Onboarding Screen

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -17,20 +17,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             Container(
               height: MediaQuery.of(context).size.height * 0.8,
               width:  MediaQuery.of(context).size.width,
-              child: Lottie.asset("assets/Lotties/1.json"),
+              child: Lottie.asset("assets/Lotties/lottie2.json"),
             ),
             GestureDetector(
               child: Container(
                 height:  MediaQuery.of(context).size.height * 0.1,
                 width:  MediaQuery.of(context).size.width * 0.7,
                 decoration: BoxDecoration(
-                  color: Colors.green[300],
+                  color: Color(0xFF161C40),
                   borderRadius: BorderRadius.all(
                     Radius.circular(20),
                   ),
                 ),
                 child: Center(
-                  child: Text("Get Started",style: TextStyle(fontSize:25,fontWeight: FontWeight.bold),),
+                  child: Text("Get Started",style: TextStyle(fontSize:25,fontWeight: FontWeight.bold,color: Colors.white),),
                 ),
               ),
               onTap: (){
